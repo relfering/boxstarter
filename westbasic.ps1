c91badcf45cd1a91b6b66d55c745cb11affff9e3
relfering <60875145+relfering@users.noreply.github.com>
2020-09-09 14:27:48 +0200

    Update westbasic.ps1

--- westbasic.ps1
+++ westbasic.ps1
@@ -44,6 +44,8 @@ function executeScript {
 #choco install -y visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
 #Update-SessionEnvironment #refreshing env due to Git install
 
+Install-WindowsUpdate -acceptEula
+if (Test-PendingReboot) { Invoke-Reboot }
 
 #--- Voeg Lokale Source Toe ---
 choco source add -n=Omroep-West -s="http://packageserver.omroep.local/chocolatey"
@@ -88,4 +90,4 @@ if (Test-PendingReboot) { Invoke-Reboot }
 #--- reenabling critial items ---
 Enable-UAC
 Enable-MicrosoftUpdate
-Install-WindowsUpdate -acceptEula
+
