ba530b3d87af4c391dcb8753de9a34290e1dd73e
relfering <60875145+relfering@users.noreply.github.com>
2020-09-11 11:16:45 +0200

    Update westbasic.ps1

--- westbasic.ps1
+++ westbasic.ps1
@@ -99,15 +99,14 @@ Enable-PSRemoting -SkipNetworkProfileCheck -Force
 #--- Company installaties ---
 #Zet NLD en Keyboard op Verenigde staten voor vfwcodecs.
 Set-WinUserLanguageList -LanguageList nl-NL -Force
+choco install MicrosoftOffice2019 -s "http://packageserver.omroep.local/chocolatey" -y
 #choco install adobephotoshopelements10 -s "http://packageserver.omroep.local/chocolatey" -y
 #choco install wacomtablet -s "http://packageserver.omroep.local/chocolatey" -y
 #choco install Audacity-2-2-2-211-GMLversion -s "http://packageserver.omroep.local/chocolatey" -y
 #choco install AstiumSwitchBoard-21-21961 -s "http://packageserver.omroep.local/chocolatey" -y
 #vanaf hier fout. Audinate driver install doet geen respons en wil rebooten.
 #choco install AudinateDanteDVS -s "http://packageserver.omroep.local/chocolatey" -y
-#Office gaat te snel. Die moet wachten tot install klaar is... omdat anders Exact de mist in gaat.
-choco install MicrosoftOffice2019 -s "http://packageserver.omroep.local/chocolatey" -y
-#choco install ExactGlobeNextInstallOnly -s "http://packageserver.omroep.local/chocolatey" -y
+choco install ExactGlobeNextInstallOnly -s "http://packageserver.omroep.local/chocolatey" -y
 choco install vfwcodecs_20 -s "http://packageserver.omroep.local/chocolatey" -y --allow-empty-checksums
 
 
